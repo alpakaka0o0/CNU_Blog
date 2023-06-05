bd6f006d418e5b83282a4ac3e628f8d5f86f0c1f
alpakaka0o0 <alpakaka000808@gmail.com>
2023-06-05 17:51:48 +0900

    resume 수정

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,4 +1,19 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return (
+    <div>
+      <h2>실전 코딩 수강</h2>
+      <p>2023.03 ~ 2023.06</p>
+      <p> backend와 frontend 실무 관련 지식 수강</p>
+      <hr />
+      <h2>2022 DEV DAY 동상 수상</h2>
+      <p>2022.11</p>
+      <p>충남대 알고리즘 대회 동상 수상</p>
+      <hr />
+      <h2>겨울 실무 캠프 2주 수강</h2>
+      <p>2022.12 ~ 2023.01</p>
+      <p>2주간 아이디어부터 개발까지 진행하는 프로그램 수강</p>
+      <hr />
+    </div>
+  );
 };
 export default Resume;
